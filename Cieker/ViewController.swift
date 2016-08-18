4b346f11a85e3d1604bf19006f7a9f8c83969710
erabala <balamurugan2999@gmail.com>
2016-08-18 15:24:43 +0530

    Initial Commit

---
+++ Cieker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cieker
+//
+//  Created by User on 18/08/16.
+//  Copyright © 2016 bala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
