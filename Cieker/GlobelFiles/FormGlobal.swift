3a467f3a21dc26e5bed9d69bf6a3c13f3d0dedb5
Bala29 <balamurugan2999@gmail.com>
2016-08-19 08:31:57 +0530

    Add NSObject Class

---
+++ Cieker/GlobelFiles/FormGlobal.swift
@@ -0,0 +1,18 @@
+//
+//  FormGlobal.swift
+//  Cieker
+//
+//  Created by Bala on 19/08/16.
+//  Copyright © 2016 bala. All rights reserved.
+//
+
+import UIKit
+
+class FormGlobal: NSObject {
+
+//    GCM :
+//    
+//    Server API Key : AIzaSyCUHXQwkSFa0EaaAQ8udBov24sCQt31UU4
+//    Sender ID : 882488677884
+    
+}
